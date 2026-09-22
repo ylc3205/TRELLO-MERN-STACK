f3a9af7145d3e4aabde99de6fd60845a69548f50
Vinh Nguyen <clytusnguyen@gmail.com>
2026-09-22 11:21:27 +0700

    fix: add preview domain to whitelist

--- trello-api/src/utils/constants.js
+++ trello-api/src/utils/constants.js
@@ -2,7 +2,6 @@ import { env } from '~/config/environment'
 
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
-  'https://trello-web-olive-rho.vercel.app',
   'https://trello-mern-stack-three.vercel.app'
 ]
 
